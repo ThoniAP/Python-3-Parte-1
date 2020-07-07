1c749dbdea3d15b731dedc9823c9b6064a292071
ThoniAP <anthoni.augusto@hotmail.com>
2020-07-07 20:12:34 -0300

    Teste

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,1 +1,15 @@
-print("hello world!")
+print("*******************************************")
+print("Bem vindo ao jogo de adivinhação")
+print("*******************************************")
+
+numero_secreto = 45
+
+numero_digitado = int(input("Digite um numero:"))
+
+if numero_secreto == numero_digitado:
+    print("Você acertou!")
+
+else:
+    print("Você errou!")
+
+print("Fim do Jogo")
