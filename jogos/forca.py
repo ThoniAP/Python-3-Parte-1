f514d7baf8fa0ada0a02e65f76fc06bce0475113
ThoniAP <ThoniAP@users.noreply.github.com>
2020-07-29 22:06:31 -0300

    Criado a função forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -5,6 +5,24 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = 'Banana'
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra?")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na prosição {}".format(chute, index))
+            index = index + 1
+
+        print("Jogando...")
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
